33c8066da6a90608657d157b28d934c992f69c50
alemuntoni <muntoni.alessandro@gmail.com>
2021-03-12 18:54:25 +0100

    fix parameters

--- scripts/macOS/2_run_paper_results.sh
+++ scripts/macOS/2_run_paper_results.sh
@@ -24,29 +24,29 @@ wait
 echo "Fabricated results computed."
 
 echo "Computing first four results..."
-./fourAxisMilling -i=$MODELS_PATH/moai.obj -o=$RESULTS_PATH/moai  > $RESULTS_PATH/moai.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/max_planck.obj -o=$RESULTS_PATH/max_planck  > $RESULTS_PATH/max_planck.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/egea.obj -o=$RESULTS_PATH/egea  > $RESULTS_PATH/egea.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/pensatore.obj -o=$RESULTS_PATH/pensatore  > $RESULTS_PATH/pensatore.log --just_segmentation 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/moai.obj -o=$RESULTS_PATH/moai --just_segmentation > $RESULTS_PATH/moai.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/max_planck.obj -o=$RESULTS_PATH/max_planck --just_segmentation > $RESULTS_PATH/max_planck.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/egea.obj -o=$RESULTS_PATH/egea --just_segmentation > $RESULTS_PATH/egea.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/pensatore.obj -o=$RESULTS_PATH/pensatore --just_segmentation > $RESULTS_PATH/pensatore.log 2>&1 &
 
 wait
 echo "First four results computed."
 
 echo "Computing second four results..."
-./fourAxisMilling -i=$MODELS_PATH/dea.obj -o=$RESULTS_PATH/dea  > $RESULTS_PATH/dea.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/maneki_neko.obj -o=$RESULTS_PATH/maneki_neko  > $RESULTS_PATH/maneki_neko.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/eros.obj -o=$RESULTS_PATH/eros  > $RESULTS_PATH/eros.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/chinese_lion.obj -o=$RESULTS_PATH/chinese_lion  > $RESULTS_PATH/chinese_lion.log --just_segmentation 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/dea.obj -o=$RESULTS_PATH/dea --just_segmentation > $RESULTS_PATH/dea.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/maneki_neko.obj -o=$RESULTS_PATH/maneki_neko --just_segmentation > $RESULTS_PATH/maneki_neko.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/eros.obj -o=$RESULTS_PATH/eros --just_segmentation > $RESULTS_PATH/eros.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/chinese_lion.obj -o=$RESULTS_PATH/chinese_lion --just_segmentation > $RESULTS_PATH/chinese_lion.log 2>&1 &
 
 
 wait
 echo "Second four results computed."
 
 echo "Computing third four results..."
-./fourAxisMilling -i=$MODELS_PATH/faget_statue.obj -o=$RESULTS_PATH/faget_statue  > $RESULTS_PATH/faget_statue.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/BU.obj -o=$RESULTS_PATH/BU  > $RESULTS_PATH/BU.log --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/3holes.obj -o=$RESULTS_PATH/3holes  > $RESULTS_PATH/3holes.log --prefiltering_smooth_iters=0 --just_segmentation 2>&1 &
-./fourAxisMilling -i=$MODELS_PATH/woman_statue.obj -o=$RESULTS_PATH/woman_statue  > $RESULTS_PATH/woman_statue.log --just_segmentation 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/faget_statue.obj -o=$RESULTS_PATH/faget_statue --just_segmentation > $RESULTS_PATH/faget_statue.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/BU.obj -o=$RESULTS_PATH/BU --just_segmentation > $RESULTS_PATH/BU.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/3holes.obj -o=$RESULTS_PATH/3holes --model_height=80 --prefiltering_smooth_iters=0 --just_segmentation > $RESULTS_PATH/3holes.log 2>&1 &
+./fourAxisMilling -i=$MODELS_PATH/woman_statue.obj -o=$RESULTS_PATH/woman_statue --just_segmentation > $RESULTS_PATH/woman_statue.log 2>&1 &
 
 wait
 echo "Third four results computed."
