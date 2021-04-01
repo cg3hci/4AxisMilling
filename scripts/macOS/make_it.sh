bef35874448149ecc4fcac5830dd705016abe971
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2021-04-01 16:27:39 +0200

    Update make_it.sh

--- scripts/macOS/make_it.sh
+++ scripts/macOS/make_it.sh
@@ -3,5 +3,5 @@
 SCRIPTS_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 sh $SCRIPTS_PATH/0_install_macos.sh
-sh $SCRIPTS_PATH//1_build.sh
-sh $SCRIPTS_PATH//2_run_paper_results.sh
+sh $SCRIPTS_PATH/1_build.sh
+sh $SCRIPTS_PATH/2_run_paper_results.sh
